7607692ebc5d6e2c410fa499bfac06be51df3408
unknown <amery.bur410@gmail.com>
2022-04-12 23:29:50 -0500

    trying to deploy to heroku.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const express = require("express");
-const session = require("express-session");
+
 const exphbs = require("express-handlebars");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
