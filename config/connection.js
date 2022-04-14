7580d4ca03295f3536b0d24ec5781257b4893204
unknown <amery.bur410@gmail.com>
2022-04-13 21:54:22 -0500

    test.

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,7 @@ if (process.env.JAWSDB_URL) {
 } else {
   sequelize = new Sequelize(process.env.DB_NAME,
      process.env.DB_USER,
-      process.env.DB_PASSORD, {
+      process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3001
