13a0a1d52fbf7d402004dd106673f82207bf4146
unknown <amery.bur410@gmail.com>
2022-04-13 21:59:03 -0500

    test.

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
-const User = require('./User');
+
 const Gallery = require('./Gallery');
 const Painting = require('./Painting');
 
@@ -10,4 +10,4 @@ Painting.belongsTo(Gallery, {
   foreignKey: 'gallery_id',
 });
 
-module.exports = { User, Gallery, Painting };
+module.exports = { Gallery, Painting };
